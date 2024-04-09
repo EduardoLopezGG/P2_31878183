7f9baa158d1f95938c3a261093449b756c6dfc60
Manuel Eduardo Prieto Lopez <eduardolopez31102005@gmail.com>
2024-04-09 16:49:00 -0400

    primera evaluacion

---
+++ PRIMERA EVALUACION/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+
+const aplicacion = express();
+
+aplicacion.use(express.static(__dirname + "/public"));
+
+aplicacion.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname + "/index.html"))
+});
+
+
+aplicacion.listen(3000, () => {
+	console.log("servidor activo en el puerto", 3000)
+});
