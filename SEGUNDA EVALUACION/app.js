6dad146398d80f8eb6ee2d9c0580b4ee205167c8
Manuel Eduardo Prieto Lopez <eduardolopez31102005@gmail.com>
2024-04-20 17:19:24 -0400

    detalles finales antes de entregar. Arreglando el app.js y finalmente enviando

---
+++ SEGUNDA EVALUACION/app.js
@@ -0,0 +1,19 @@
+//Creamos dos constantes, una que requiera express y otra que requiera path
+const express = require("express");
+const path = require("path");
+
+//creamos la variable aplicacion con los valores de express
+const aplicacion = express();
+
+//Ubicamos los archivos estaticos del sitio web
+aplicacion.use(express.static(__dirname + "/public"));
+
+//ubicamos el o los archivos html para mostrar correctamoente la pagina
+aplicacion.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname + "/CV.html"))
+});
+
+//hace que la variable aplicacion escuche y haga funcionamiento en el puerto 3000
+aplicacion.listen(3000, () => {
+	console.log("servidor activo en el puerto", 3000)
+});
