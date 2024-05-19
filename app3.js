37689f687621c544cbf0a85b5cccb3a6ba748f87
Manuel Eduardo Prieto Lopez <eduardolopez31102005@gmail.com>
2024-05-19 19:14:59 -0400

    actualizacion 2.2

--- app3.js
+++ app3.js
@@ -10,40 +10,6 @@ aplicacion.get("/", (req, res) => {
 	res.sendFile(path.join(__dirname + "/index.html"))
 });
 
-const db = new sqlite3.Database('contactos.db');
-
-db.run(`
-    CREATE TABLE IF NOT EXISTS contactos (
-        id INTEGER PRIMARY KEY,
-        nombre TEXT,
-        email TEXT,
-        comentario TEXT,
-        ip_usuario TEXT,
-        fecha_hora DATETIME
-    )
-`);
-
-app.use(express.urlencoded({ extended: true }));
-
-app.post('/', (req, res) => {
-  const { nombre, email, comentario } = req.body;
-  const ip_usuario = req.ip; // Dirección IP del usuario
-  const fecha_hora = new Date();
-
-  // Guardar datos en la base de datos
-  db.run(`
-      INSERT INTO contactos (nombre, email, comentario, ip_usuario, fecha_hora)
-      VALUES (?, ?, ?, ?, ?)
-  `, [nombre, email, comentario, ip_usuario, fecha_hora], (err) => {
-      if (err) {
-          console.error(err.message);
-          res.status(500).send('Error al guardar los datos');
-      } else {
-          res.send('¡Gracias por contactarnos!');
-      }
-  });
-});
-
 aplicacion.listen(3000, () => {
 	console.log("servidor activo en el puerto", 3000)
 });
